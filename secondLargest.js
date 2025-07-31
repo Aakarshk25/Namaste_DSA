0ff9ad29e59cf0ddfc9a686ea2b7f5195a73e139
Aakarsh Srivastava <132938129+Aakarshk25@users.noreply.github.com>
2025-07-31 21:57:40 +0530

    Create secondLargest.js

---
+++ secondLargest.js
@@ -0,0 +1,24 @@
+// Find Second largest number in a array
+
+
+function secondLargest(arr) {
+    firstLargest = -Infinity;
+    secondLargest = -Infinity;
+
+    for (i = 0; i < arr.length; i++){
+        if (arr[i] > firstLargest) {
+            secondLargest = firstLargest;
+            firstLargest = arr[i];
+        }
+        else if(arr[i] > secondLargest){
+            secondLargest = arr[i];
+        }
+    }
+    return secondLargest;
+}
+
+let arr = [4, 9, 0, 2, 8, 7, 1];
+
+let result = secondLargest(arr);
+//console.log(result)
+
